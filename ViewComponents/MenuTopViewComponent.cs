a8755a8ce0f537f72d033788fdde211a559c5d18
ngotrianh <anhblaks@gmail.com>
2024-11-25 21:47:08 +0700

    17

---
+++ ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nhom17.Models;
+
+namespace nhom17.ViewComponents
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        private readonly Nhom17Context _context;
+        public MenuTopViewComponent(Nhom17Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbMenus.Where( m => (bool) m.IsActive).
+                OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
---
+++ ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using nhom17.Models;
+
+namespace nhom17.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly Nhom17Context _context;
+       
+        public ProductViewComponent(Nhom17Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbProducts.Include(m => m.CategoryProduct)
+                .Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+            return await Task.FromResult<IViewComponentResult>
+                (View(items.OrderByDescending(m => m.ProductId).ToList()));
+        }
+    }
+}
