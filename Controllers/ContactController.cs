7efc77274e83706734776f755d3c1446681ca26e
ngotrianh <anhblaks@gmail.com>
2024-11-29 14:07:35 +0700

    nhom17

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nhom17.Models;
+
+namespace nhom17.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        private readonly Nhom17Context _context;
+
+        public ContactController(Nhom17Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(string name,string phone,string email,string message)
+        {
+            try
+            {
+                TbContact contact = new TbContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;
+                contact.Message = message;
+                contact.CreatedDate = DateTime.Now;
+                _context.Add(contact);
+                _context.SaveChangesAsync();
+                return Json(new { status = true });
+            }
+            catch
+            {
+                return Json(new {status = false });
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/nhom17.AssemblyInfo.cs
+++ obj/Debug/net8.0/nhom17.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nhom17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a9b37cf953402aba6ba79940f9246179a95268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ee4bb759e2467db80d830615006f35cd292862")]
 [assembly: System.Reflection.AssemblyProductAttribute("nhom17")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nhom17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
